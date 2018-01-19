e62eea5163e04835d32ba32df27bf11e6a454b62
Daniel Werthen <danielwerthen@gmail.com>
2018-01-19 12:26:56 +0100

    That is not needed?

--- src/meta-object.js
+++ src/meta-object.js
@@ -4,9 +4,6 @@ function MetaObject() {
 
 MetaObject.prototype = new Proxy(MetaObject.prototype, {
   get(target, property, receiver) {
-    if (Reflect.has(receiver, property)) {
-      return target[property]
-    }
     if (Reflect.has(receiver, 'methodMissing')) {
       const method = receiver.methodMissing(property)
       if (method !== void 0) {
