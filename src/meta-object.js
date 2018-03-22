733fdb4a2ba076fb99d6fbe4be568a7e2935e122
Daniel Werthen <danielwerthen@gmail.com>
2018-03-22 18:55:08 +0100

    Es5 again

--- src/meta-object.js
+++ src/meta-object.js
@@ -3,7 +3,7 @@
 function MetaObject() {}
 
 MetaObject.prototype = new Proxy(MetaObject.prototype, {
-  get(target, property, receiver) {
+  get: function get(target, property, receiver) {
     if (Reflect.has(receiver, 'methodMissing')) {
       var method = receiver.methodMissing(property);
       if (method !== void 0) {
